c0f5940f199636812823423441f9e954882a40c2
jajinmori <jajinmori1@naver.com>
2022-05-12 18:01:01 +0900

    chapter_04_gradient

--- Deep_Learning_from_Scratch/Chapter_04.py
+++ Deep_Learning_from_Scratch/Chapter_04.py
@@ -133,3 +133,42 @@ def function_tmp1(x0):
 numerical_diff(function_tmp1, 3.0)
 
 # %%
+def numerical_gradient(f, x):
+  h = 1e-4
+  grad = np.zeros_like(x)
+
+  for idx in range(x.size):
+    tmp_val = x[idx]
+    # f(x+h) 계산
+  
+    x[idx] = tmp_val + h
+    fxh1 = f(x)
+
+    # f(x-h) 계산
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+
+    grad[idx] = (fxh1 - fxh2) / (2*h)
+    x[idx] = tmp_val
+
+  return grad
+# %%
+numerical_gradient(function_2, np.array([3.0,4.0]))
+
+numerical_gradient(function_2, np.array([0.0,2.0]))
+
+numerical_gradient(function_2, np.array([3.0,0.0]))
+# %%
+def gradient_descent(f, init_x, lr=0.01, step_num = 100):
+  x = init_x
+  for i in range(step_num):
+    grad = numerical_gradient(f,x)
+    x -= lr * grad
+  return x
+# %%
+def function_2(x):
+  return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100)
+# %%
