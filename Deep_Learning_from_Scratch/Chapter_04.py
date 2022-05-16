9c26107f23091a6e9020a54eb87273d81ffebc70
jajinmori <jajinmori1@naver.com>
2022-05-16 17:28:01 +0900

    Chapter_04_finished

--- Deep_Learning_from_Scratch/Chapter_04.py
+++ Deep_Learning_from_Scratch/Chapter_04.py
@@ -1,40 +1,53 @@
-#%%
-y = [0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0]
-t = [0,0,1,0,0,0,0,0,0,0]
+# %%
+from argparse import ONE_OR_MORE
+from functions import *
+from gradient import numerical_gradient
+from functions import softmax, cross_entropy_error
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pylab as plt
+from mnist import load_mnist
+import numpy as np
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
 
 def mean_squared_error(y, t):
-  return 0.5*np.sum((y-t)**2)
+    return 0.5*np.sum((y-t)**2)
+
 # %%
-import numpy as np
+
 
 mean_squared_error(np.array(y), np.array(t))
 # %%
 
 # %%
-y = [0.1,0.05,0.1,0.0,0.05,0.1,0.0,0.6,0.0,0.0]
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 mean_squared_error(np.array(y), np.array(t))
 # %%
+
+
 def cross_entropy_error(y, t):
-  delta = 1e-7
-  return -np.sum(t*np.log(y+delta))
+    delta = 1e-7
+    return -np.sum(t*np.log(y+delta))
 
-t = [0,0,1,0,0,0,0,0,0,0]
-y = [0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0]
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
 cross_entropy_error(np.array(y), np.array(t))
 
-y = [0.1,0.05,0.1,0.0,0.05,0.1,0.0,0.6,0.0,0.0]
-cross_entropy_error(np.array(y),np.array(t))
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+cross_entropy_error(np.array(y), np.array(t))
 # %%
-from mnist import load_mnist
 
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+(x_train, t_train), (x_test, t_test) = load_mnist(
+    normalize=True, one_hot_label=True)
 
 print(x_train.shape)
 print(t_train.shape)
 
 
 # %%
-import numpy as np
 
 train_size = x_train.shape[0]
 batch_size = 10
@@ -42,37 +55,43 @@ batch_mask = np.random.choice(train_size, batch_size)
 x_batch = x_train[batch_mask]
 t_batch = t_train[batch_mask]
 # %%
-#* 미니배치 교차 엔트로피 오차 구현
-def cross_entropy_error(y, t):
-  if y.ndim == 1:
-    t = t.reshape(1, t.size)
-    y = y.reshape(1, y.size)
+# * 미니배치 교차 엔트로피 오차 구현
 
-  batch_size = y.shape[0]
-  return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
 
 # %%
-#* not원핫인코딩, 숫자레이블인경우 교차 엔트로피 오차 구현
+# * not원핫인코딩, 숫자레이블인경우 교차 엔트로피 오차 구현
+
+
 def cross_entropy_error(y, t):
-  if y.ndim == 1:
-    t = t.reshape(1, t.size)
-    y = y.reshape(1, y.size)
-    
-  batch_size = y.shape[0]
-  return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 # %%
 # 나쁜 구현의 예
+
+
 def numerical_diff(f, x):
-  h = 1e-4
-  return (f(x+h) - f(x-h)) /(2*h)
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
 
-#np.float32(1e-50)
+# np.float32(1e-50)
 # %%
-def function_1(x):
-  return 0.01*x**2 + 0.1*x
 
-import numpy as np
-import matplotlib.pylab as plt
+
+def function_1(x):
+    return 0.01*x**2 + 0.1*x
+
 
 x = np.arange(0.0, 20.0, 0.1)
 y = function_1(x)
@@ -82,92 +101,108 @@ plt.plot(x, y)
 plt.show()
 
 # %%
-numerical_diff(function_1,5)
+numerical_diff(function_1, 5)
 numerical_diff(function_1, 10)
 # %%
-def function_2(x):
-  return x[0]**2 + x[1]**2
-  # 또는 return np.sum(x**2)
-# %%
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pyplot as plt
-import numpy as np
 
-def f(x,y):
-  r = 2*x**2 + y**2
-  ans = r*np.exp(-r)
-  return ans
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+    # 또는 return np.sum(x**2)
+# %%
+
+
+def f(x, y):
+    r = 2*x**2 + y**2
+    ans = r*np.exp(-r)
+    return ans
+
 
 xn = 10
-x = np.linspace(-3,3,xn)
-y = np.linspace(-3,3,xn)
-temp = np.zeros((len(x),len(y)))
+x = np.linspace(-3, 3, xn)
+y = np.linspace(-3, 3, xn)
+temp = np.zeros((len(x), len(y)))
 for i in range(xn):
-  for j in range(xn):
-    temp[j,i] = f(x[i],y[j])
+    for j in range(xn):
+        temp[j, i] = f(x[i], y[j])
 
-xx,yy = np.meshgrid(x,y)
+xx, yy = np.meshgrid(x, y)
 
-plt.figure(figsize=(9,9))
-ax = plt.subplot(111,projection='3d')
-ax.plot_surface(xx,yy,temp)
+plt.figure(figsize=(9, 9))
+ax = plt.subplot(111, projection='3d')
+ax.plot_surface(xx, yy, temp)
 plt.show()
 # %%
-fig = plt.figure(figsize = (9,6))
+fig = plt.figure(figsize=(9, 6))
 ax = fig.add_subplot(111, projection='3d')
 
-def f(x,y):
-  return x**2 + y**2
 
-xn= 100
-x = np.array([1,2,3,4,5])
-y = np.array([1,2,3,4,5])
+def f(x, y):
+    return x**2 + y**2
+
+
+xn = 100
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 2, 3, 4, 5])
 z = x**2 + y**2
-ax.plot(x,y,z)
+ax.plot(x, y, z)
 print(z)
 # %%
 #! 편미분, 목표변수를 제외한 변수는 상수로 취급!
+
+
 def function_tmp1(x0):
-  return x0*x0 + 4.0**2.0
+    return x0*x0 + 4.0**2.0
+
 
 numerical_diff(function_tmp1, 3.0)
 
 # %%
+
+# gradient 기울기, 
 def numerical_gradient(f, x):
-  h = 1e-4
-  grad = np.zeros_like(x)
+    h = 1e-4
+    grad = np.zeros_like(x)
 
-  for idx in range(x.size):
-    tmp_val = x[idx]
-    # f(x+h) 계산
-  
-    x[idx] = tmp_val + h
-    fxh1 = f(x)
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
 
-    # f(x-h) 계산
-    x[idx] = tmp_val - h
-    fxh2 = f(x)
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
 
-    grad[idx] = (fxh1 - fxh2) / (2*h)
-    x[idx] = tmp_val
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
 
-  return grad
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+
 # %%
-numerical_gradient(function_2, np.array([3.0,4.0]))
+numerical_gradient(function_2, np.array([3.0, 4.0]))
 
-numerical_gradient(function_2, np.array([0.0,2.0]))
+numerical_gradient(function_2, np.array([0.0, 2.0]))
 
-numerical_gradient(function_2, np.array([3.0,0.0]))
+numerical_gradient(function_2, np.array([3.0, 0.0]))
 # %%
-def gradient_descent(f, init_x, lr=0.01, step_num = 100):
-  x = init_x
-  for i in range(step_num):
-    grad = numerical_gradient(f,x)
-    x -= lr * grad
-  return x
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
 # %%
+
+
 def function_2(x):
-  return x[0]**2 + x[1]**2
+    return x[0]**2 + x[1]**2
+
 
 init_x = np.array([-3.0, 4.0])
 gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100)
@@ -180,27 +215,25 @@ gradient_descent(function_2, init_x=init_x, lr=10.0, step_num=100)
 init_x = np.array([-3.0, 4.0])
 gradient_descent(function_2, init_x=init_x, lr=1e-10, step_num=100)
 # %%
-import numpy as np
-#from common.functions import softmax, cross_entropy_error
-#from common.gradient import numerical_gradient
+
 
 class simpleNet:
-  def __init__(self):
-    self.w = np.random.randn(2,3) # 정규분포로 초기화
+    def __init__(self):
+        self.W = np.random.randn(2, 3)  # 정규분포로 초기화
 
-  def predict(self, x):
-    return np.dot(x, self.w)
+    def predict(self, x):
+        return np.dot(x, self.W)
 
-  def loss(self, x, t):
-    z = self.predict(x)
-    y = softmax(z)
-    loss = cross_entropy_error(y, t)
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
 
-    return loss
+        return loss
 
 
 net = simpleNet()
-print(net.w)
+print(net.W)
 
 x = np.array([0.6, 0.9])
 p = net.predict(x)
@@ -208,6 +241,176 @@ print(p)
 
 np.argmax(p)
 
-t = np.array([0,0,1])
-net.loss(x,t)
+t = np.array([0, 0, 1])
+net.loss(x, t)
 # %%
+
+
+# %%
+def f(W): 
+  return net.loss(x, t)
+
+
+dW = numerical_gradient(f, net.W)
+print(dW)
+
+# %%
+f = lambda w: net.loss(x, t)
+eW = numerical_gradient(f, net.W)
+#%% 
+from functions import *
+from gradient import numerical_gradient
+
+class TwoLayerNet:
+  def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+
+    #* 가중치 초기화
+    self.params = {}
+    self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+    self.params['b1'] = np.zeros(hidden_size)
+    self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+    self.params['b2'] = np.zeros(output_size)
+
+  def predict(self, x):
+    W1, W2 = self.params['W1'], self.params['W2']
+    b1, b2 = self.params['b1'], self.params['b2']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    y = softmax(a2)
+
+    return y
+
+  # x : 입력 데이터, t : 정답 레이블
+  def loss(self, x, t):
+    y = self.predict(x)
+
+    return cross_entropy_error(y, t)
+
+  def accuracy(self, x, t):
+    y = self.predict(x)
+    y = np.argmax(y, axis=1)
+    t = np.argmax(t, axis=1)
+
+    accuracy = np.sum(y == t) / float(x.shape[0])
+    return accuracy
+    
+  #* x : 입력데이터, t : 정답 레이블
+  def numerical_gradient(self, x, t):
+    loss_W = lambda W: self.loss(x, t)
+
+    grads = {}
+    grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+    grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+    grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+    grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+    
+    return grads
+# %%
+net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+net.params['W1'].shape
+net.params['b1'].shape
+net.params['W2'].shape
+net.params['b2'].shape
+# %%
+x = np.random.rand(100, 784) # 더미 입력 데이터(100장 분량)
+y = net.predict(x)
+# %%
+
+x = np.random.rand(100, 784) # 더미 입력 데이터(100장 분량)
+t = np.random.rand(100, 10) # 더미 정답 레이블(100장 분량)
+
+grads = net.numerical_gradient(x, t) # 기울기 계산
+
+grads['W1'].shape
+# %%
+grads['b1'].shape
+# %%
+grads['W2'].shape
+# %%
+grads['b2'].shape
+# %%
+#! 미니배치 학습 구현
+import numpy as np
+from mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+#*하이퍼파라미터
+iters_num = 10000 #반복회수
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+  # 미니배치 획득
+  batch_mask = np.random.choice(train_size, batch_size)
+  x_batch = x_train[batch_mask]
+  t_batch = t_train[batch_mask]
+
+  # 기울기 계산
+  grad = network.numerical_gradient(x_batch, t_batch)
+  # grad = network.gradient(x_batch, t_batch) # 성능 새헌판!
+
+  # 매개변수 갱신
+  for key in ('W1', 'b1', 'W2', 'b2'):
+    network.params[key] -= learning_rate * grad[key]
+
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+# %%
+#! 미니배치 학습 구현 (수정)
+import numpy as np
+from mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size = 784, hidden_size=50, output_size = 10)
+
+#*하이퍼파라미터
+iters_num = 10000 #반복회수
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# 1에폭당 반복수
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+  # 미니배치 획득
+  batch_mask = np.random.choice(train_size, batch_size)
+  x_batch = x_train[batch_mask]
+  t_batch = t_train[batch_mask]
+
+  # 기울기 계산
+  grad = network.numerical_gradient(x_batch, t_batch)
+  # grad = network.gradient(x_batch, t_batch) # 성능 개선판!
+
+  # 매개변수 갱신
+  for key in ('W1', 'b1', 'W2', 'b2'):
+    network.params[key] -= learning_rate * grad[key]
+
+  # 학습 경과 기록
+  loss = network.loss(x_batch, t_batch)
+  train_loss_list.append(loss)
+
+  # 1에폭당 정확도 계산
+  if i % iter_per_epoch == 0:
+    train_acc = network.accuracy(x_train, t_train)
+    test_acc = network.accuracy(x_test, t_test)
+    train_acc_list.append(train_acc)
+    test_acc_list.append(test_acc)
+    print("train acc, test acc | "
+          + str(train_acc) + ", " + str(test_acc))
+
+# %%
+
