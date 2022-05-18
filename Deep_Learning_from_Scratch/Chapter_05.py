781479c18f70eadd4c08f94fdf11583b48d76183
jajinmori <jajinmori1@naver.com>
2022-05-18 18:01:20 +0900

    Chapter_05

--- Deep_Learning_from_Scratch/Chapter_05.py
+++ Deep_Learning_from_Scratch/Chapter_05.py
@@ -116,3 +116,98 @@ print(x)
 mask = (x <= 0)
 print(mask)
 # %%
+x[mask] = 0
+x[mask]
+
+
+# %%
+x = np.array([[1.0, -0.5], [-2.0, 3.0]])
+abc = (x <= 0)
+out = x.copy()
+out[abc] = 0
+print(out)
+# %%
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
+# %%
+X = np.random.rand(2)  # 입력
+W = np.random.rand(2, 3)  # 가중치
+B = np.random.rand(3)  # 편향
+
+X.shape  # (2, )
+W.shape  # (2, 3)
+B.shape  # (3, )
+
+Y = np.dot(X, W) + B
+# %%
+X_dot_W = np.array([[0, 0, 0], [10, 10, 10]])
+B = np.array([1, 2, 3])
+
+X_dot_W
+X_dot_W + B
+# %%
+dY = np.array([[1, 2, 3], [4, 5, 6]])
+dY
+dB = np.sum(dY, axis=0)
+dB
+# %%
+
+
+class Affine:
+    def __init_(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+# %%
+
+
+class SoftWithLoss:
+    def __init__(self):
+        self.loss = None  # 손실
+        self.y = None  # softmax의 출력
+        self.t = None  # 정답 레이블(원-핫 벡터)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = corss_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
+# %%
